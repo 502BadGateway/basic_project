6bd73a0765ef2a8e83e05571338c89d2b611d99a
Dan Beglin <dbeglinuk@gmail.com>
2014-10-29 10:09:10 +0000

    Added Documentation / Variable names conform with Spec
    I have added annotations to the code and I have renamed the variables to fit in with the best practice for coding that we agreed on.

--- basic_project.py
+++ basic_project.py
@@ -1,16 +1,28 @@
-from Tkinter import*
-window = Tk()
-arena = Canvas(window, width = 500, height = 500, bg = 'white')
+"""
+set up window
+"""
+
+from Tkinter import * # imports everything from the Tkinter library
+window = Tk() # generates a window 
+arena = Canvas(window, width = 500, height = 500, bg = 'white') # generates a canvas of 500px x 500px for the arena
 arena.pack()
-key = Canvas(window, width = 500, height = 100, bg = 'grey')
+key = Canvas(window, width = 500, height = 100, bg = 'grey') # generates a canvas for our key and UI area
 key.pack()
-objectrectangle1 = arena.create_rectangle(350, 50, 400, 200, fill = "red")
-objectrectangle2 = arena.create_rectangle(150, 200, 300, 250, fill = "red")
-objectrectangle3 = arena.create_rectangle(400, 250, 500, 300, fill = "red")
-objectrectangle4 = arena.create_rectangle(0, 350, 250, 400, fill = "red")
-objectrectangle5 = arena.create_rectangle(400, 350, 450, 400, fill = "red")
-objectrectangle6 = arena.create_rectangle(0, 0, 100, 200, fill = "red")
-objectrectangle7 = arena.create_rectangle(0, 450, 50, 500, fill = "#00FF99")
 
-window.mainloop()
+"""
+objects in arena
+"""
 
+obstacle_rectangle1 = arena.create_rectangle(350, 50, 400, 200, fill = "red") # creates a obstacle to avoid
+obstacle_rectangle2 = arena.create_rectangle(150, 200, 300, 250, fill = "red")
+obstacle_rectangle3 = arena.create_rectangle(400, 250, 500, 300, fill = "red")
+obstacle_rectangle4 = arena.create_rectangle(0, 350, 250, 400, fill = "red")
+obstacle_rectangle5 = arena.create_rectangle(400, 350, 450, 400, fill = "red")
+obstacle_rectangle6 = arena.create_rectangle(0, 0, 100, 200, fill = "red")
+start_area = arena.create_rectangle(0, 450, 50, 500, fill = "#00FF99") # uses a Hexidecimal code for light green
+
+"""
+run program
+"""
+
+window.mainloop() # runs everything
