f5cb01fcdd564426b167d2b98b76a5aa66e1afc7
Adenuga Banjoko <banjokoa@uni.coventry.ac.uk>
2014-10-31 01:02:23 +0000

    Update basic_project.py

--- basic_project.py
+++ basic_project.py
@@ -13,13 +13,13 @@ key.pack()
 objects in arena
 """
 
-obstacle_rectangle1 = arena.create_rectangle(350, 50, 400, 200, fill = "red") # creates a obstacle to avoid
-obstacle_rectangle2 = arena.create_rectangle(150, 200, 300, 250, fill = "red")
-obstacle_rectangle3 = arena.create_rectangle(400, 250, 500, 300, fill = "red")
-obstacle_rectangle4 = arena.create_rectangle(0, 350, 250, 400, fill = "red")
-obstacle_rectangle5 = arena.create_rectangle(400, 350, 450, 400, fill = "red")
-obstacle_rectangle6 = arena.create_rectangle(0, 0, 100, 200, fill = "red")
-start_area = arena.create_rectangle(0, 450, 50, 500, fill = "#00FF99") # uses a Hexidecimal code for light green
+obstacle_rectangle1 = arena.create_rectangle(350, 50, 400, 200, fill = "red", outline = "red") # creates a obstacle to avoid
+obstacle_rectangle2 = arena.create_rectangle(150, 200, 300, 250, fill = "red", outline = "red")
+obstacle_rectangle3 = arena.create_rectangle(400, 250, 500, 300, fill = "red", outline = "red")
+obstacle_rectangle4 = arena.create_rectangle(0, 350, 250, 400, fill = "red", outline = "red")
+obstacle_rectangle5 = arena.create_rectangle(400, 350, 450, 400, fill = "red", outline = "red")
+obstacle_rectangle6 = arena.create_rectangle(0, 0, 100, 200, fill = "red", outline = "red")
+start_area = arena.create_rectangle(0, 450, 50, 500, fill = "#00FF99", outline = "#00FF99") # uses a Hexidecimal code for light green
 
 #flag gif
 gif1 = PhotoImage(file = 'flag.gif')
